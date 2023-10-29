6ddabd04026e0538d9a68dda655b5ffb9f181e95
Alexandr <alexandr.pogorelov91@gmail.com>
2023-10-29 19:44:10 +0700

    работа с методами

---
+++ Task1-Mod5/Task1-Mod5/Program.cs
@@ -0,0 +1,179 @@
+﻿namespace Task1_Mod5
+{
+    internal class Program
+    {
+        static string ShowColor()
+        {
+            
+            Console.WriteLine("Напишите свой любимы цвет на английском с меленькой буквы");
+
+            var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш цвет красный!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш цвет зеленый!");
+                    break;
+
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Ваш любимы цвет берюзовый!");
+                    break;
+
+                default:
+
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Blue;
+
+                    Console.WriteLine("Ваш любимый цвет желтый!");
+                    break;
+
+
+            }
+
+            return color;
+        }
+
+       static string GetDataFromConsole() => Console.ReadLine();
+
+        static int[] GetArrayFromConsole()
+        {
+            var resoult = new int[5];
+                        
+            for (int i = 0; i < resoult.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0} ", i + 1);
+                resoult[i] = int.Parse(Console.ReadLine());                                               
+            }
+
+            //Сортировка массива
+            int temp;
+
+            for (int i = 0; i < resoult.Length; i++)
+            {
+                for (int j = i + 1; j < resoult.Length; j++)
+                {
+                    if (resoult[i] > resoult[j])
+                    {
+                        temp = resoult[i];
+                        resoult[i] = resoult[j];
+                        resoult[j] = temp;
+                    }
+                }
+            }
+            //---------------------------------------
+            /*//Сортировка одномерного массива
+
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+
+                        arr[i] = arr[j];
+
+                        arr[j] = temp;
+                    }
+                    
+                }
+            }
+
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+                              
+            }
+            */
+            //---------------------------------------
+
+            for (int i = 0; i < resoult.Length; i++)
+            {
+                Console.WriteLine(resoult[i]);
+            }
+                        
+            return resoult;
+        }
+
+        static void Main(string[] args)
+        {
+
+            GetArrayFromConsole();
+            //-----------------------------------------------------------------------
+
+            (string Name, string[] Dishes) User;
+
+            User.Dishes = new string[5];
+
+            Console.WriteLine("Введите свое имя: ");
+            User.Name = Console.ReadLine();
+
+            Console.WriteLine("Введите 5 своих любимых блюд.");
+            for (int i = 0; i <= User.Dishes.Length; i++)
+            {                
+                
+                if (i < User.Dishes.Length)
+                {
+                    Console.Write("Блюдо {0}: ", i + 1);
+                    User.Dishes[i] = Console.ReadLine();
+                }
+                
+            }
+            Console.WriteLine("Список ваших любимых блюд: ");
+
+            foreach (var resoult in User.Dishes)
+            {
+                Console.WriteLine(resoult);
+            }
+
+            //-----------------------------------------------------------------------
+            var UserColors = new string[3];
+
+            for (int i = 0; i < UserColors.Length; i++)
+            {
+                UserColors[i] = ShowColor();
+                
+            }
+
+            Console.WriteLine("Ваши любимые цвета: ");
+
+            foreach (var resoultColor in UserColors)
+            {
+                Console.WriteLine(resoultColor);
+            }
+
+            Console.ReadLine();
+
+            //-----------------------------------------------------------------------
+
+            
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
