abde2c510304468d333c69e6dee0901ab97946c6
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-02-01 23:48:40 +0700

    5.3 Работа с рекурсивными функциями

--- Task1-Mod5/Task 5.5/Task 5.5/Program.cs
+++ Task1-Mod5/Task 5.5/Task 5.5/Program.cs
@@ -4,19 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Напиште что-то");
-            var str = Console.ReadLine();
+            //Console.WriteLine("Напиште что-то");
+            //var str = Console.ReadLine();
 
-            Console.WriteLine("Укажите глубину эха");
-            var deep = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Укажите глубину эха");
+            //var deep = int.Parse(Console.ReadLine());
 
-            Echo(str, deep);
+
+            //Echo(str, deep);
+
+            //Console.WriteLine(Factorial(20));
+
+            Console.WriteLine(PowerUp(2, 3));
 
             Console.ReadKey();
 
-
         }
 
+       
         static void Echo(string saidworld, int deep)
         {
             var modif = saidworld;
@@ -25,6 +30,9 @@
             {
                 modif = modif.Remove(0, 2);
             }
+
+
+            Console.BackgroundColor = (ConsoleColor)deep;
             Console.WriteLine("..." + modif);
 
             if (deep > 1)
@@ -32,6 +40,39 @@
                 Echo(modif, deep - 1);
             }
 
+        }
+
+       static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        public static int PowerUp(int N, byte pow)
+        {
+            if ( pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+                       
+            
         }
 
 
