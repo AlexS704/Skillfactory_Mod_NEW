f58cfa7a3db3c36f28eb5125a2a81974eb36edf4
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-02-08 21:55:49 +0700

    6.2: Создание классов Pen, Reactange

---
+++ Mod6/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Mod6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+
+        }
+
+    }
+
+    class Rectangle
+    {
+        public int a;
+        public int b;
+
+        public int Square()
+        {
+            return a * b;                       
+        }
+
+        public Rectangle(int first, int second)
+        {
+            a = first;
+            b = second;
+        }
+
+        public Rectangle(int side)
+        {
+            a = side;
+            b = side;
+        }
+
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+
+
+
+    }
+
+
+
+
+}
