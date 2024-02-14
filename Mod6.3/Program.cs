5eb2c39f2172d4038de3d68b734143acc625a2dc
AlexandrPogorelov <alexandr.pogorelov91@gmail.com>
2024-02-14 23:52:54 +0700

    Работа по модулю

---
+++ Mod6.3/Program.cs
@@ -0,0 +1,85 @@
+﻿namespace Mod6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var department = GetCurrentDepartment();
+        }
+
+        static Department GetCurrentDepartment()
+        {
+            // logic;
+        }
+
+        class Company
+        {
+            public string Type = "Банк";
+            public string Name;
+        }
+
+        class Department
+        {
+            public Company Company;
+            public City City;
+        }
+
+        class City
+        {
+            public string Name = "Санкт-Петербург";
+        }
+
+        class Triangle
+        {
+            public double a;
+            public double b;
+            public double c;
+
+            public void Perimeter(double a, double b, double c)
+            {
+                double result = a + b + c;
+            }
+
+            public void Area(double a, double b, double c)
+            {
+                double semiperimeter = (a + b + c)/2;
+
+                double result = Math.Sqrt(semiperimeter*(semiperimeter - a)*(semiperimeter - b)*(semiperimeter - c));
+
+            }
+
+        }
+
+        class Circle
+        {
+            public double radius;
+
+            public void Circumference(double radius)
+            {
+                double result = 2*Math.PI*radius;
+            }
+
+            public void Area(double radius)
+            {
+                double result = 2 * Math.PI * Math.Pow(radius,2);
+            }
+        }
+
+        class Square
+        {
+            public double a;
+
+            public void Area(double a)
+            {
+                double result = a * a;
+            }
+
+            public void Perimeter(double a)
+            {
+                double result = a * 4;
+            }
+
+        }
+    }
+
+}
--- Mod6/Program.cs
+++ Mod6/Program.cs
@@ -5,6 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            Data data = new Data {Name = "Запись", Length = 10, Version = 1, Array = new int[] {15, 30} };
+            Obj obj = new Obj {Name = "Стол", IsAlive = false, Weight = 15 };
+
+            var dataCopy = data;
+            var objCopy = obj;
+
+            data.Name = "Значение";
+            data.Length = 35;
+            data.Version = 2;
+            data.Array[0] = 0;
+
+            obj.Name = "Кот";
+            obj.IsAlive = true;
+            obj.Weight = 3;
+
+            objCopy = new Obj { Name = obj.Name, IsAlive = obj.IsAlive, Weight = obj.Weight };
+
+            obj.Name = "Стол";
+            obj.IsAlive = false;
+            obj.Weight = 15;
+
+            Console.ReadKey();
+
         }
     }
 
@@ -60,7 +84,22 @@
 
     }
 
+    struct Data
+    {
+        public string Name;
+        public int Length;
+        public int Version;
+        public int[] Array;
 
+    }
+
+    class Obj
+    {
+        public string Name;
+        public bool IsAlive;
+        public int Weight;
+
+    }
 
 
 }
