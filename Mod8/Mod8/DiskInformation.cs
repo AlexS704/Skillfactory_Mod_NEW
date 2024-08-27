d5e6cb4f0697ba675288669d8ae54a9367df117e
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-08-27 13:59:07 +0700

    Работа над классами модуля 8.2

---
+++ Mod8/Mod8/DiskInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod8
+{
+    internal class DiskInformation
+    {
+        public DiskInformation(string NameDisk, long SizeDisc, long FreeSpaceDisc)
+        {
+            NameDisk = _nameDisc;
+            SizeDisc = _sizeDisc;
+            FreeSpaceDisc = _freeSpaceDisc;
+        
+        }
+
+        public string _nameDisc {  get; }
+        public long _sizeDisc {  get; }
+        public long _freeSpaceDisc {  get; }
+
+    }
+}
---
+++ Mod8/Mod8/Folder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod8
+{
+    public class Folder
+    {
+        public List<string> Files {  get; set; } = new List<string>();
+
+        public string CreateNewDirectory(string nameFolder)
+        {
+            string result = "";
+
+            return result;        
+        }
+    }
+}
