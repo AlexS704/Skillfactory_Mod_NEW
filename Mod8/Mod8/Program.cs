7ecbdfe8ea5fdc4a9a5fa47149b31618a168b18b
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-09-03 14:05:33 +0700

    Работа по 8.2.2

--- Mod8/Mod8/Program.cs
+++ Mod8/Mod8/Program.cs
@@ -34,21 +34,58 @@
                 Console.WriteLine("Папки:");
                 string[] dirs = Directory.GetFiles(dirName); //получение всех директорий корневого каталога
 
+
+                int countFolders = 0;
                 foreach (string d in dirs) //выводим все директории
                 {
                     Console.WriteLine(d);
+                   countFolders++;                    
                 }
+
+                Console.WriteLine($"Количество папок в списке: {countFolders}");
 
                 Console.WriteLine();
                 Console.WriteLine("Файлы:");
+
+                int countFiles = 0;
                 string[] files = Directory.GetFiles(dirName); //Получаем все файлы корневого каталога
 
                 foreach (string f in files) //Вывод файлов
                 {
                     Console.WriteLine(f);
+                    countFiles++;
                 }
+
+                Console.WriteLine($"Количество файлов в списке: {countFiles}");
+
+                int result = countFolders + countFiles;
+                Console.WriteLine($"Всего объектов:{result}");
+
             }
+
+            ////через конструкцию try/catch
+            //try
+            //{
+            //    DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\");
+            //    if (dirInfo.Exists)
+            //    {
+            //        Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+            //    }
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);
+            //}
+
+            //через создание новой директории
+            DirectoryInfo dirInfo = new DirectoryInfo(@"С:\Users\luft");
+            if (!dirInfo.Exists)
+                dirInfo.Create();
+            dirInfo.CreateSubdirectory("NewFolder");
+
         }
+
+
 
     }
 }
