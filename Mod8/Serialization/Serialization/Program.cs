0e5bb62640e9bf9976c32ba84772f82bd8ea8cce
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-09-17 23:29:58 +0700

    Работа с модулями

---
+++ Mod8/Serialization/Serialization/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+namespace Serialization
+{
+    [Serializable]
+    class Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Pet(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //объект для сериализации
+            var pet = new Pet("Rex", 2);
+            Console.WriteLine("Объект создан");
+
+            //Сериализация
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            var jsonString = JsonSerializer.Serialize(pet, options);
+            File.WriteAllText("myPets.json", jsonString);
+            Console.WriteLine("Объект сериализован");
+
+            //Дессериализация
+            jsonString = File.ReadAllText("myPets.json");
+            var newPet = JsonSerializer.Deserialize<Pet>(jsonString);
+            Console.WriteLine("Объект десериализован");
+            Console.WriteLine($"Имя: {newPet.Name} ----------Возраст: {newPet.Age}");
+        }
+    }
+
+
+}
