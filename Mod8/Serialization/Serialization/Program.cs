261180b05f848008680595e9ac3a1b1630e29692
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-09-18 13:51:33 +0700

    работа с 8.4.3

--- Mod8/Serialization/Serialization/Program.cs
+++ Mod8/Serialization/Serialization/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text.Json;
 namespace Serialization
 {
@@ -37,6 +38,41 @@ namespace Serialization
             var newPet = JsonSerializer.Deserialize<Pet>(jsonString);
             Console.WriteLine("Объект десериализован");
             Console.WriteLine($"Имя: {newPet.Name} ----------Возраст: {newPet.Age}");
+
+            //------------------------------------------------------------------------
+
+            Contact contact = new Contact("Иван", 79995554563, "sd@gmail.com");
+            Console.WriteLine("Объект contact создан");
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("Contact.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, contact);
+                Console.WriteLine("Объект contact сериализован");
+            }
+
+
+            using(FileStream fs = new FileStream("contact.dat", FileMode.Open.Opencrea))
+            {
+
+            }
+
+        }
+    }
+
+    [Serializable]
+    public class Contact
+    {
+        public string Name { get; set; }
+        public long PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Contact(string name, long phoneNumber, string email)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+            Email = email;
         }
     }
 
