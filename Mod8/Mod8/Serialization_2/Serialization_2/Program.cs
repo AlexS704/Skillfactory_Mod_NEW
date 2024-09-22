09e274449d56e36bd30c8d87c913cd62ea029556
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-09-22 11:54:43 +0700

    Смена имения Решения

---
+++ Mod8/Mod8/Serialization_2/Serialization_2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+
+public class Contact
+{
+    public string Name { get; set; }
+    public long PhoneNumber { get; set; }
+    public string Email { get; set; }
+
+    public Contact(string name, long phoneNumber, string email)
+    {
+        Name = name;
+        PhoneNumber = phoneNumber;
+        Email = email;
+    }
+}
+
+//public class BinarySerializer : IBinaryFormatter<Contact>
+//{
+//    public byte[] Serialize(Contact contact)
+//    {
+//        using MemoryStream stream = new MemoryStream();
+//        using BinaryWriter writer = new BinaryWriter(stream);
+
+//        writer.Write(contact.Name);
+//        writer.Write(contact.PhoneNumber);
+//        writer.Write(contact.Email);
+
+//        return stream.ToArray();
+//    }
+
+//    public Contact Deserialize(byte[] bytes)
+//    {
+//        using MemoryStream stream = new MemoryStream(bytes);
+//        using BinaryReader reader = new BinaryReader(stream);
+
+//        string name = reader.ReadString();
+//        long phoneNumber = reader.ReadInt64();
+//        string email = reader.ReadString();
+
+//        return new Contact(name, phoneNumber, email);
+//    }
+//}
+
+
+
+
+
+namespace Serialization_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Contact contact = new Contact("Ivan", 89991112254, "dadad@faf.com");
+            //BinarySerializer serializer = new BinarySerializer();
+
+            //byte[] bytes = serializer.Serialize(contact);
+            //Contact deserializedContact = serializer.Deserialize(bytes);
+
+            //Console.WriteLine($"Name: {deserializedContact.Name}");
+            //Console.WriteLine($"PhoneNumber: {deserializedContact.PhoneNumber}";
+            //Console.WriteLine($"Email: {deserializedContact.Email}");
+
+        }
+    }
+
+}
+
+
+
+
+
--- Mod8/Serialization/Serialization/Program.cs
+++ Mod8/Serialization/Serialization/Program.cs
@@ -46,60 +46,12 @@ namespace Serialization
             Console.WriteLine($"Имя: {newPet.Name} ----------Возраст: {newPet.Age}");
 
             //------------------------------------------------------------------------
-            Contact contact = new Contact("Ivan", 89991112254,"dadad@faf.com");
-            BinarySerializer serializer = new BinarySerializer();
-
-            byte[] bytes = serializer.Serialize(contact);
-            Contact deserializedContact = serializer.Deserialize(bytes);
-
-            Console.WriteLine($"Name: {deserializedContact.Name}");
-            Console.WriteLine($"PhoneNumber: {deserializedContact.PhoneNumber}";
-            Console.WriteLine($"Email: {deserializedContact.Email}");
             
 
         }
     }
         
-    public class Contact
-    {
-        public string Name { get; set; }
-        public long PhoneNumber { get; set; }
-        public string Email { get; set; }
-
-        public Contact(string name, long phoneNumber, string email)
-        {
-            Name = name;
-            PhoneNumber = phoneNumber;
-            Email = email;
-        }
-    }
-
-    public class BinarySerializer : IBinaryFormatter<Contact>
-    {
-        public byte[] Serialize(Contact contact)
-        {
-            using MemoryStream stream = new MemoryStream();
-            using BinaryWriter writer = new BinaryWriter(stream);
-
-            writer.Write(contact.Name);
-            writer.Write(contact.PhoneNumber);
-            writer.Write(contact.Email);
-            
-            return stream.ToArray();
-        }
-
-        public Contact Deserialize(byte[] bytes)
-        {
-            using MemoryStream stream = new MemoryStream(bytes);
-            using BinaryReader reader = new BinaryReader(stream);
-
-            string name = reader.ReadString();
-            long phoneNumber = reader.Readlong();
-            string email = reader.ReadString();
-
-            return new Contact(name, phoneNumber, email);
-        }
-    }
+    
 
 }
 
