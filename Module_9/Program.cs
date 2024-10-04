122deaf9752dfa31b66b37704756bea8444eb3e2
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-10-04 13:55:49 +0700

    Работа с модулем 9

--- Module_9/Program.cs
+++ Module_9/Program.cs
@@ -1,5 +1,37 @@
 ﻿
-Exception exception = new Exception("Ошибка");
 
-exception.Data.Add("Дата создания:", DateTime.Now);
-exception.HelpLink = "www.Google.com";
+//Exception exception = new Exception("Ошибка");
+
+//exception.Data.Add("Дата создания:", DateTime.Now);
+//exception.HelpLink = "www.Google.com";
+
+//try
+//{       
+//       throw new ArgumentOutOfRangeException("Аргумент находится за пределами диапазона");
+//}
+//catch (ArgumentOutOfRangeException ex)
+//{
+
+//    Console.WriteLine(ex.Message );
+
+//}
+//finally
+//{
+//    Console.ReadKey();
+
+//}
+
+try
+{
+    throw new RankException("Сообщение об ошибке");
+}
+
+catch (RankException ex)
+{
+    Console.WriteLine(ex.GetType());
+}
+
+finally
+{
+    Console.Read();
+}
