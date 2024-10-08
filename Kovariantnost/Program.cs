40d62a19edf309efa57c32068debb8fb5c47d7e7
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-10-08 13:57:00 +0700

    Работа с заданиями модуля 9

---
+++ Kovariantnost/Program.cs
@@ -0,0 +1,57 @@
+﻿public static Car LexusHandler()
+{
+    return null;
+}
+
+public static Lexus LexusHandler()
+{
+    return null;
+}
+
+public static void GetParentInfo(Parent p)
+{
+    Console.WriteLine(p.GetType());
+}
+
+public static Parent ParentHandler()
+{
+    return null;
+}
+
+public static Child ChildHandler()
+{
+    return null;
+}
+
+//ковариантность
+CarHandler carHandler = LexusHandler;
+Console.Read();
+
+//контрвариантность
+ChildInfo childInfo = GetParentInfo;
+childInfo.Invoke(new Child());
+
+public delegate Car CarHandler();
+
+public delegate Parent HandlerMethod();
+delegate void ChildInfo(Child child);
+
+public class Car
+{
+
+}
+
+public class Lexus : Car
+{
+
+}
+//--------------------------------------------
+class Parent
+{
+
+}
+
+class Child: Parent
+{
+
+}
