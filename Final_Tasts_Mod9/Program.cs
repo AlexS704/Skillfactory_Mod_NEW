bb9cc2ac3bb37901fc8c96007f2e34f24865d569
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-10-14 21:22:24 +0700

    Практикум мод9

---
+++ Final_Tasts_Mod9/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Final_Tasts_Mod9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+
+    public class MyException : Exception
+    {
+        public MyException(string message) : base(message)
+        {
+
+        
+        
+        }
+    }
+}
