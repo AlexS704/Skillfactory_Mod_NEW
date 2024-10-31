ca977db50c7ea9f8888cd1aa3ce8acf6e0455aeb
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2024-10-31 13:53:16 +0700

    Работа с заданиями модуля 10

---
+++ Mod10-Tasks1/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Mod10_Tasks1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFile file = new FileManager();
+            IBinaryFile binaryFile = new FileManager();
+            FileManager fileManager = new FileManager();
+
+            file.ReadFile();
+            binaryFile.ReadBinary();
+            fileManager.OpenSource();
+
+            Console.ReadKey();
+        }
+    }
+   //переделать имена интерфейсов
+    public interface IFile
+    {
+        void ReadFile();
+    }
+
+    public interface IBinaryFile
+    {
+        void ReadBinary();
+
+        void OpenBinaryFile();
+    }
+
+    public class FileManager : IFile, IBinaryFile
+    {
+        void IFile.ReadFile()
+        {
+            Console.WriteLine("Чтение файла....");
+        }
+
+        void IBinaryFile.ReadBinary()
+        {
+            Console.WriteLine("Чтение бинарного файла....");
+        }
+
+        void IBinaryFile.OpenBinaryFile()
+        {
+            Console.WriteLine("Открытие бинарного файла");
+        }
+
+        public void OpenSource()
+        {
+            Console.WriteLine("Открытие строки поиска");
+        }
+    }
+}
---
+++ Mod10/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Mod10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Writer writer = new Writer();
+
+            ((IWriter)writer).Write();
+            Console.ReadKey();
+
+            var worker = new Worker();
+
+            ((IWorker)worker).Build();
+           
+        }
+    }
+
+    public class Writer : IWriter
+    {
+        void IWriter.Write()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Worker : IWorker
+    {
+        void IWorker.Build()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    
+    public interface IWriter 
+    {
+        void Write();
+    }
+
+    public interface IWorker
+    {
+        public void Build();
+    }
+}
