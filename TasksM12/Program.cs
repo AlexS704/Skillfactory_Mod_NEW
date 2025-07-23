9ae9f31d3706e0f144993957132c1c250369db7a
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-07-23 13:43:34 +0700

    12.1.3; 12.1.4

---
+++ TasksM12/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void Greetings()
+{
+    Console.WriteLine("Как Вас зовут" ?);
+    var userName = Console.ReadLine();
+    var greetings = "Привет," + userName;
+    Console.WriteLine(greetings);
+}
+
+static void ArrayFilling()
+{
+    Console.WriteLine("Сколько элементов будет в массиве?");
+    var count = Int32.Parse(Console.ReadLine());
+    var array = new string[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = Console.ReadLine();
+    }
+
+    Console.WriteLine("Все элементы записаны");
+}
---
+++ TasksMod12/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TasksMod12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            static void Greetings()
+            {
+                Console.WriteLine("Как Вас зовут" ?);
+                var userName = Console.ReadLine();
+                var greetings = "Привет," + userName;
+                Console.WriteLine(greetings);
+            }
+
+            static void ArrayFilling()
+            {
+                Console.WriteLine("Сколько элементов будет в массиве?")
+                int count = Console.ReadLine();
+                array = new int[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    array[i];
+                }
+
+
+            }
+
+        }
+    }
+}
