8b79c23b3add9882a6ac89be4622e5dc787944ad
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-07-24 13:45:50 +0700

    task 12.1.5

--- TasksM12/Program.cs
+++ TasksM12/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TasksM12;
+
 static void Greetings()
 {
-    Console.WriteLine("Как Вас зовут" ?);
+    Console.WriteLine("Как Вас зовут?");
     var userName = Console.ReadLine();
     var greetings = "Привет," + userName;
     Console.WriteLine(greetings);
@@ -19,4 +21,49 @@ static void ArrayFilling()
     }
 
     Console.WriteLine("Все элементы записаны");
+}
+
+static void AgeVerification()
+{
+    Console.WriteLine("Введите свой возраст: ");
+    var age = Int32.Parse(Console.ReadLine());
+
+    if (age > 13)
+    {
+        Console.WriteLine("Вы успешно зарегистрированы");
+    }
+    else { Console.WriteLine("Пользователи младше 14 лет не могут быть зерегистрированы"); }
+}
+
+//Задание 12.1.5
+static void ShowAds()
+{
+    Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+    // Остановка на 1 с
+    Thread.Sleep(1000);
+
+    Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+    // Остановка на 2 с
+    Thread.Sleep(2000);
+
+    Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+    // Остановка на 3 с
+    Thread.Sleep(3000);
+}
+
+User user = new User();
+
+user.Name = "Александр";
+user.IsPremium = false;
+
+string greeting = "Добро пожаловать " + user.Name;
+
+if (user.IsPremium == true)
+{
+    Console.WriteLine(greeting);
+}
+else
+{
+    Console.WriteLine(greeting);
+    ShowAds();
 }
---
+++ TasksM12/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksM12
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public bool IsPremium { get; set; }
+        
+    }
+}
