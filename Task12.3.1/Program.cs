21485951c5ee33e54b6df3ab197560d1823bef38
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-07-28 13:52:21 +0700

    Реализация задач по модулю

---
+++ Task12.3.1/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] array = { 1, 3, 5, 6, 6, 6, 7, 9};
+int value = 1;
+Console.WriteLine(BinarySearch(value, array));
+
+
+
+
+static int BinarySearch(int value, int[] array)
+{
+    int left = 0;
+    int right = array.Length - 1;    
+
+    while (left <= right)
+    {
+        int middle = left + (right - left)/2;
+        int midElement = array[middle];
+
+        if(midElement == value)
+        {
+            return middle;
+        }
+
+        else if (value < midElement)
+        {
+            right = middle - 1;
+        }
+
+        else
+        {
+            left = middle + 1;
+        }
+    }
+
+    return -1;
+}
