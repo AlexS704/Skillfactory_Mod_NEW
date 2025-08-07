d8edaf4e19613ef77bd54212e3e92ea6e668a14f
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-08-07 14:01:14 +0700

    Работа над заданиями 13 модуля

---
+++ Mod13/TasksMod13/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace TasksMod13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(GetArray(new[] {1, 2, 3, 4, 5}));
+
+            //Console.WriteLine(GetArray(new[] { 6, 5, 3, 4, 5 }));
+
+            //13.1.6
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);            
+
+            if (File.Exists("cdev_Text.txt"))
+            {
+                Console.WriteLine("Файл сущствует");
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден");
+            }
+        }
+
+        //13.1.4
+        static bool GetArray(int[] array)
+        {
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    return false;
+                }                            
+            }
+            return true;
+        }
+    }
+}
