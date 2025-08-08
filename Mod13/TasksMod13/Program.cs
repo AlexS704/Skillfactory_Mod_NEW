c44a20bee641bb6275f51c18854940be8e2d7a0e
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-08-08 13:59:24 +0700

    Реализация задачи 13.1.6

--- Mod13/TasksMod13/Program.cs
+++ Mod13/TasksMod13/Program.cs
@@ -9,16 +9,36 @@
             //Console.WriteLine(GetArray(new[] { 6, 5, 3, 4, 5 }));
 
             //13.1.6
-            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);            
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //получаем доступ к раб.столу             
 
-            if (File.Exists("cdev_Text.txt"))
+            string fileName = "cdev_Text.txt";
+            string filePath = Path.Combine(desktopPath, fileName); //объединяем два фрагмента пути (путь к рабочему столу и имя файла)=полный путь к файлу
+
+            if (File.Exists(filePath))
             {
-                Console.WriteLine("Файл сущствует");
+                try
+                {
+                    string content = File.ReadAllText(filePath); //читаем содержимое файла(не подходит для больших >1Гб - лучше использовать StreamReader)
+
+                    int worldCount = CountWords(content);
+
+                    Console.WriteLine($"Файл найден: {filePath}");
+                    Console.WriteLine($"Количество слов в файле: {worldCount}");
+                }
+                catch (IOException ex) { Console.WriteLine($"Ошибка при чтении файла: {ex.Message}"); }
             }
             else
             {
                 Console.WriteLine("Файл не найден");
             }
+        }
+
+        //метод подсчета слов
+        static int CountWords(string content)
+        {
+            //Удаляем лишние пробелы и разбиваем текст на слова
+            char[] separatorsText = new char[] { ' ', '\t', '\n', '\r' }; //получаем массив разделителей
+            return content.Split(separatorsText, StringSplitOptions.RemoveEmptyEntries).Length; //получаем массив слов без разделителей и возвращаем длину этого массива
         }
 
         //13.1.4
