e894b865844bcbb0f410a2d7067561461ebd629a
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-08-14 13:02:44 +0700

    код задания 13.2.6

--- Mod13/TasksMod13/Program.cs
+++ Mod13/TasksMod13/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace TasksMod13
+﻿using System.Collections;
+using System.Text;
+
+namespace TasksMod13
 {
     internal class Program
     {
@@ -9,27 +12,73 @@
             //Console.WriteLine(GetArray(new[] { 6, 5, 3, 4, 5 }));
 
             //13.1.6
-            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //получаем доступ к раб.столу             
+            //string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //получаем доступ к раб.столу             
 
-            string fileName = "cdev_Text.txt";
-            string filePath = Path.Combine(desktopPath, fileName); //объединяем два фрагмента пути (путь к рабочему столу и имя файла)=полный путь к файлу
+            //string fileName = "cdev_Text.txt";
+            //string filePath = Path.Combine(desktopPath, fileName); //объединяем два фрагмента пути (путь к рабочему столу и имя файла)=полный путь к файлу
 
-            if (File.Exists(filePath))
+            //if (File.Exists(filePath))
+            //{
+            //    try
+            //    {
+            //        string content = File.ReadAllText(filePath); //читаем содержимое файла(не подходит для больших >1Гб - лучше использовать StreamReader)
+
+            //        int worldCount = CountWords(content);
+
+            //        Console.WriteLine($"Файл найден: {filePath}");
+            //        Console.WriteLine($"Количество слов в файле: {worldCount}");
+            //    }
+            //    catch (IOException ex) { Console.WriteLine($"Ошибка при чтении файла: {ex.Message}"); }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Файл не найден");
+            //}
+
+            //13.2.6
+            //Напишите метод, который на вход принимает любой Arraylist input, а на выходе выдает другой Arraylist с 
+            //двумя элементами, где первый — число(сумма целочисленных элементов input), а второй — строка(текст, составленный из строковых элементов input).
+
+
+            var arrayList = new ArrayList()
             {
-                try
+                1,
+                "Андрей ",
+                "Сергей ",
+                300,
+            };
+
+            //переменная для хранения суммы
+            int sum = 0;
+
+            // переменная для хранения текста.
+            // Можно было бы использовать String, но в случае когда необходимо выполнять много
+            // операций с одной строкой - лучше использовать класс StringBuilder
+            StringBuilder text = new StringBuilder();
+
+            //проходим список и проверям элементы на соответствие типу
+            foreach (var element in arrayList)
+            {
+                //если целое число - увеличиваем счетчик
+                if (element is int)
                 {
-                    string content = File.ReadAllText(filePath); //читаем содержимое файла(не подходит для больших >1Гб - лучше использовать StreamReader)
-
-                    int worldCount = CountWords(content);
-
-                    Console.WriteLine($"Файл найден: {filePath}");
-                    Console.WriteLine($"Количество слов в файле: {worldCount}");
+                    sum += (int)element;
                 }
-                catch (IOException ex) { Console.WriteLine($"Ошибка при чтении файла: {ex.Message}"); }
+
+                //если строка - добавляем текст из нее
+                if(element is string s)
+                {
+                    text.Append(element);
+                }
             }
-            else
+
+            //результат
+            var result = new ArrayList() { sum, text.ToString() };
+
+            //вывод
+            foreach (var elem in result)
             {
-                Console.WriteLine("Файл не найден");
+                Console.WriteLine(elem);
             }
         }
 
@@ -54,5 +103,9 @@
             }
             return true;
         }
+
+    
+
+
     }
 }
