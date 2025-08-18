423856c792f1485c73cddf1938c92e5f3407c79f
Alexandr Pogorelov <alexandr.pogorelov91@gmail.com>
2025-08-19 00:01:22 +0700

    задание 13.3.4

---
+++ Mod13/Task.Mod.13.3.4/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Task.Mod._13._3._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Contact> phoneBook = new List<Contact>()
+            {
+               new Contact ("Иванов", 1, "A123" ),
+            };
+
+            Console.WriteLine("Введите Имя: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Введите Телефон: ");
+            int userPhone = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите Почту: ");
+            var userMail = Console.ReadLine();
+
+            Contact contact = new Contact(userName, userPhone, userMail);
+           
+            //List<Contact> phoneBook;
+
+            AddUnique(contact, phoneBook);
+            
+        }
+
+        private static void AddUnique(Contact newContact, List<Contact> phoneBook)
+        {
+            bool alreadyExists = false;
+
+            foreach (var contact in phoneBook)
+            {
+                if (contact.Name == newContact.Name
+                    & contact.PhoneNumber == newContact.PhoneNumber
+                    & contact.Email == newContact.Email)
+                {
+                    alreadyExists = true;
+                    break;
+                }                
+            }
+
+            if (!alreadyExists)
+                phoneBook.Add(newContact);
+
+            phoneBook.Sort((x, y) => String.Compare(x.Name, y.Name, StringComparison.Ordinal));
+
+            foreach (var contact in phoneBook)
+            {
+                Console.WriteLine(contact.Name + ": " + contact.PhoneNumber + ": "
+                    + contact.Email + ": ");
+            }
+        }
+    }
+}
